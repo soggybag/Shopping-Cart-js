9012948bc4d22d92f8408305aa566f347177870a
Mitchell Hudson <soggybag@gmail.com>
2015-06-09 22:16:57 -0700

    initial commit
    A simple JavaScript Shopping Cart

---
+++ js/shoppingCart.js
@@ -0,0 +1,124 @@
+// ***************************************************
+// Shopping Cart functions 
+
+var shoppingCart = (function () {
+    // Private methods and properties
+    var cart = [];
+
+    function Item(name, price, count) {
+        this.name = name
+        this.price = price
+        this.count = count
+    }
+    
+    function saveCart() {
+        localStorage.setItem("shoppingCart", JSON.stringify(cart));
+    }
+
+    function loadCart() {
+        this.cart = JSON.parse(localStorage.getItem("shoppingCart"));
+    }
+
+    loadCart();
+    
+    
+
+    // Public methods and properties
+    var obj = {};
+    
+    obj.addItemToCart = function (name, price, count) {
+        for (var i in cart) {
+            if (cart[i].name === name) {
+                cart[i].count += count;
+                saveCart();
+                return;
+            }
+        }
+
+        console.log("addItemToCart:", name, price, count);
+
+        var item = new Item(name, price, count);
+        cart.push(item);
+        saveCart();
+    };
+    
+    obj.setCountForItem = function (name, count) {
+        for (var i in cart) {
+            if (cart[i].name === name) {
+                cart[i].count = count;
+                break;
+            }
+        }
+        saveCart();
+    };
+    
+    
+    obj.removeItemFromCart = function (name) { // Removes one item
+        for (var i in cart) {
+            if (cart[i].name === name) { // "3" === 3 false
+                cart[i].count--; // cart[i].count --
+                if (cart[i].count === 0) {
+                    cart.splice(i, 1);
+                }
+                break;
+            }
+        }
+        saveCart();
+    };
+    
+    
+    obj.removeItemFromCartAll = function (name) { // removes all item name
+        for (var i in cart) {
+            if (cart[i].name === name) {
+                cart.splice(i, 1);
+                break;
+            }
+        }
+        saveCart();
+    };
+    
+    
+    obj.clearCart = function () {
+        cart = [];
+        saveCart();
+    }
+    
+    
+    obj.countCart = function () { // -> return total count
+        var totalCount = 0;
+        for (var i in cart) {
+            totalCount += cart[i].count;
+        }
+
+        return totalCount;
+    };
+    
+    obj.totalCart = function () { // -> return total cost 
+        var totalCost = 0;
+        for (var i in cart) {
+            totalCost += cart[i].price * cart[i].count;
+        }
+        return totalCost.toFixed(2);
+    };
+    
+    obj.listCart = function () { // -> array of Items
+        var cartCopy = [];
+        for (var i in cart) {
+            var item = cart[i];
+            var itemCopy = {};
+            for (var p in item) {
+                itemCopy[p] = item[p];
+            }
+            itemCopy.total = (item.price * item.count).toFixed(2);
+            cartCopy.push(itemCopy);
+        }
+        return cartCopy;
+    };
+    
+    // ----------------------------
+    return obj;
+})();
+
+
+
+
